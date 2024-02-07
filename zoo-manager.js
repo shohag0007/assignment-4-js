aa3136ce251600b2cc4a9364c826fac9dac0d033
shohag0007 <merinedrive007@gmail.com>
2024-02-07 20:46:22 +0600

    Assignment

---
+++ zoo-manager.js
@@ -0,0 +1,15 @@
+// ( 10 * 120) - ( 500 + (8 * 50) ) = 300
+
+function calculateMoney(ticketSale){
+    const ticket = 0;
+    const ticketPrice = 120;
+    const sales  = ticket * ticketPrice;
+    const stuffCost = 8 * 50 + 500;
+    const balance = sales - stuffCost;
+    total = balance;
+    return total;
+    
+    }
+
+ ticketSale([500]);
+// console.log(calculateMoney);
