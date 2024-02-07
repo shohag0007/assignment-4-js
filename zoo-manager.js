b2f7e7112a0914963d82ec9802b32434f0083634
shohag0007 <merinedrive007@gmail.com>
2024-02-08 01:07:49 +0600

    out of five three is done

--- zoo-manager.js
+++ zoo-manager.js
@@ -1,15 +1,42 @@
-// ( 10 * 120) - ( 500 + (8 * 50) ) = 300
-
-function calculateMoney(ticketSale){
-    const ticket = 0;
-    const ticketPrice = 120;
-    const sales  = ticket * ticketPrice;
-    const stuffCost = 8 * 50 + 500;
-    const balance = sales - stuffCost;
-    total = balance;
-    return total;
-    
+function calculateMoney(ticketSale) {
+    if (0 <= ticketSale) {
+        const result = (ticketSale * 120) - (500 + (8 * 50));
+        return result;
     }
+    else {
+        return "Invalid Number";
+    }
+}
 
- ticketSale([500]);
-// console.log(calculateMoney);
+
+function checkName(name) {
+
+    if (typeof name === 'string') {
+        const lastChar = name.substring(name.length - 1);
+        if (lastChar === 'a' || lastChar === 'y' || lastChar === 'i' || lastChar === 'e' || lastChar === 'o' || lastChar === 'u' || lastChar === 'w' || lastChar === 'A' || lastChar === 'Y' || lastChar === 'I' || lastChar === 'E' || lastChar === 'O' || lastChar === 'U' || lastChar === 'W') {
+            return "Good Name";
+        }
+        else {
+            return "Bad Name";
+        }
+    }
+    else {
+        return "invalid";
+    }
+}
+
+
+function deleteInvalids(array) {
+    
+    if(typeof array === "array") {
+        const filterArray = array.filter(item => typeof item === 'number' && !isNaN(item));
+    return filterArray;
+    }
+    else {
+        return "Invalid Array";
+    }
+}
+
+const test = deleteInvalids({num: [ 1 , 2 , 3 ]}  );
+
+console.log(test);
