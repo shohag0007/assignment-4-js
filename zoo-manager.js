ce2c03a36aadff1a44e5e32dc085bd321a33d83b
shohag0007 <merinedrive007@gmail.com>
2024-02-09 00:15:07 +0600

    Five problems are solved for Assignmet-4.js

--- zoo-manager.js
+++ zoo-manager.js
@@ -10,7 +10,6 @@ function calculateMoney(ticketSale) {
 
 
 function checkName(name) {
-
     if (typeof name === 'string') {
         const lastChar = name.substring(name.length - 1);
         if (lastChar === 'a' || lastChar === 'y' || lastChar === 'i' || lastChar === 'e' || lastChar === 'o' || lastChar === 'u' || lastChar === 'w' || lastChar === 'A' || lastChar === 'Y' || lastChar === 'I' || lastChar === 'E' || lastChar === 'O' || lastChar === 'U' || lastChar === 'W') {
@@ -27,16 +26,49 @@ function checkName(name) {
 
 
 function deleteInvalids(array) {
-    
-    if(typeof array === "array") {
+    if (typeof array === "array") {
         const filterArray = array.filter(item => typeof item === 'number' && !isNaN(item));
-    return filterArray;
+        return filterArray;
     }
     else {
         return "Invalid Array";
     }
 }
 
-const test = deleteInvalids({num: [ 1 , 2 , 3 ]}  );
 
-console.log(test);
+function password(obj) {
+    if (!obj.name || !obj.siteName || obj.birthYear.toString().length < 4) {
+        return "invalid"
+    }
+    else {
+        const generatePassword = obj.siteName.charAt(0).toUpperCase() + obj.siteName.slice(1, obj.siteName.length) + '#' + obj.name + '@' + obj.birthYear;
+        return generatePassword;
+    }
+}
+
+
+function monthlySavings(arr, livingCost) {
+    if (typeof arr !== "object" || typeof livingCost !== 'number') {
+        return 'invalid input';
+    }
+    else {
+        let savings = 0;
+        let totalEarn = 0;
+        for (i = 0; i < arr.length; i++) {
+            if (3000 <= arr[i]) {
+                totalEarn = totalEarn + (arr[i] - (arr[i] * 20) / 100);
+            }
+            else {
+                totalEarn = totalEarn + arr[i];
+            }
+        }
+        savings = totalEarn - livingCost;
+        if (0 <= savings) {
+            return savings;
+        }
+        else {
+            return "earn  more";
+        }
+
+    }
+}
